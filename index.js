e1a20bf4b9a51a5b39718759a03234f8f5c2c8c6
federicotiralongo95@gmail.com <federicotiralongo95@gmail.com>
2021-05-27 17:56:13 +0200

    arreglo de algunos detalle

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const esItaliano = (apellido) =>{
     if (apellido[apellido.length - 1] === "i"){
         return `${apellido} :Este apellidotiene muchas probabilidades de ser italiano`;
     } else if(apellido[apellido.length - 1 ] !== "i"){
-        return`${apellido} :Este apellido podria ser de cualquier pais`
+        return`${apellido} :Este apellido podria ser de cualquier pais`;
     }
 } 
 console.log(esItaliano(apellido1)); 
